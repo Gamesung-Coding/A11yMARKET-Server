6d3bb6417ec3d1d85389582539efdea8806839be
BlueNyang <xpsj20@gmail.com>
2025-11-07 16:31:38 +0900

    feat: add user manage features for admin
    - add mapping into controller to inquire all users
    - add service and implementation for inquire Users.
    - this commit wouldn't include User Entity and User Repository

---
+++ src/main/java/com/multicampus/gamesungcoding/a11ymarketserver/admin/user/service/AdminUserManageService.java
@@ -0,0 +1,9 @@
+package com.multicampus.gamesungcoding.a11ymarketserver.admin.user.service;
+
+import com.multicampus.gamesungcoding.a11ymarketserver.user.model.User;
+
+import java.util.List;
+
+public interface AdminUserManageService {
+    List<User> listAll();
+}
