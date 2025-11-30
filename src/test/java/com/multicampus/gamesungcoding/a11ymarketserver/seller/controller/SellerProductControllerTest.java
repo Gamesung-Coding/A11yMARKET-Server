5cf051425146e1e16217a007195c9a13b9a753d3
BlueNyang <dksrbxo@bluenyang.kr>
2025-12-01 02:24:57 +0900

    test: add service unit test

--- src/test/java/com/multicampus/gamesungcoding/a11ymarketserver/seller/controller/SellerProductControllerTest.java
+++ src/test/java/com/multicampus/gamesungcoding/a11ymarketserver/seller/controller/SellerProductControllerTest.java
@@ -1,4 +1,20 @@
 package com.multicampus.gamesungcoding.a11ymarketserver.seller.controller;
 
+import com.multicampus.gamesungcoding.a11ymarketserver.feature.seller.controller.SellerController;
+import com.multicampus.gamesungcoding.a11ymarketserver.feature.seller.service.SellerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+@Import(SellerController.class)
+@WebMvcTest(SellerController.class)
 public class SellerProductControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+    @MockitoBean
+    private SellerService sellerService;
+
+
 }
