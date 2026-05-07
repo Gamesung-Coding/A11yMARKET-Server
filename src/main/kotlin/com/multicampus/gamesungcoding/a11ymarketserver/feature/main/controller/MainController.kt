ae49028b047062408d180c16a5e5d1be8d780ad6
BlueNyang <dksrbxo@bluenyang.kr>
2026-05-07 23:30:16 +0900

    migrate: controller for mainpages features

---
+++ src/main/kotlin/com/multicampus/gamesungcoding/a11ymarketserver/feature/main/controller/MainController.kt
@@ -0,0 +1,28 @@
+package com.multicampus.gamesungcoding.a11ymarketserver.feature.main.controller
+
+import com.multicampus.gamesungcoding.a11ymarketserver.feature.main.dto.CategoryRecommendResponse
+import com.multicampus.gamesungcoding.a11ymarketserver.feature.main.dto.EventResponse
+import com.multicampus.gamesungcoding.a11ymarketserver.feature.main.entity.MonthlyPopularProduct
+import com.multicampus.gamesungcoding.a11ymarketserver.feature.main.service.MainService
+import lombok.RequiredArgsConstructor
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/v1/main/")
+class MainController(
+    private val mainService: MainService
+) {
+
+    @GetMapping("products/populars")
+    fun popularProducts(): List<MonthlyPopularProduct> = mainService.findTop10ByOrderByRankingAsc()
+
+
+    @GetMapping("products/categories")
+    fun categories(): List<CategoryRecommendResponse> = mainService.getAllCategories()
+
+
+    @GetMapping("events")
+    fun events(): List<EventResponse> = mainService.getAllEvents()
+}
